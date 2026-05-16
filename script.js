5d1bf9bbae01c39764a8e6464fe76103ade27dae
Riya Shrestha <shrer488@newschool.edu>
2026-05-16 00:37:26 -0400

    adding modal designs

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const showDescription = document.getElementById(".showDescription");
-const dialog = document.getElementById(".description");
-const closeDialog = document.getElementById(".closeDescription");
+const modalButton = document.querySelector("#modal");
+const modalDialog = document.querySelector("#dialog");
+const closeDialog = modalDialog.querySelector(".closeModal");
 
 
-console.log(showDescription);
-showDescription.addEventListener("click", () => {
-  dialog.showModal();
+console.log(modalButton);
+modalButton.addEventListener("click", () => {
+  modalDialog.showModal();
 });
 
 closeDialog.addEventListener("click", () => {
-  dialog.close();
+  modalDialog.close();
 });
+
+// Listen to *all* clicks, now including the `event` parameter…
+document.addEventListener('click', (event) => {
+	// Only clicks on the page itself behind the `dialog`.
+	if (event.target == document.documentElement) {
+		modalDialog.close() // Close it too then.
+	}
+})
